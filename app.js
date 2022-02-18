0c810625154ea1e483e0077caa0dea0bce59af76
Tomas Coelho <tomascoe.wd@gmail.com>
2022-02-18 17:50:59 -0600

    switching reverse geocoding to google api

--- app.js
+++ app.js
@@ -28,51 +28,15 @@ let weaherApi = {
 };
 
 const geocode = {
-  reverseGeocode: (latitude, longitude) => {
-    var api_key = "fd84533b87dd4e7f9341e370188d1f1b";
-    var api_url = "https://api.opencagedata.com/geocode/v1/json";
-
-    var request_url =
-      api_url +
-      "?" +
-      "key=" +
-      api_key +
-      "&q=" +
-      encodeURIComponent(latitude + "," + longitude) +
-      "&pretty=1" +
-      "&no_annotations=1";
-
-    // see full list of required and optional parameters:
-    // https://opencagedata.com/api#forward
-
-    var request = new XMLHttpRequest();
-    request.open("GET", request_url, true);
-
-    request.onload = function () {
-      // see full list of possible response codes:
-      // https://opencagedata.com/api#codes
-
-      if (request.status === 200) {
-        // Success!
-        console.log("reverseGeocode | " + data.results[0].components.suburb); // PRINT LOCATION SUBURB
-        weaherApi.fetchWeather(data.results[0].components.suburb);
-      } else if (request.status <= 500) {
-        // We reached our target server, but it returned an error
-
-        console.log("unable to geocode! Response code: " + request.status);
-        var data = JSON.parse(request.responseText);
-        console.log("error msg: " + data.status.message);
-      } else {
-        console.log("server error");
-      }
-    };
-
-    request.onerror = function () {
-      // There was a connection error of some sort
-      console.log("unable to connect to server");
-    };
-
-    request.send(); // make the request
+  reverseGeocode: async function (latitude, longitude) {
+    const geocodeKey = "AIzaSyBVOoSLlkW2Y5uSoNPl5MBR7gd4BkH7VDQ";
+    let geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${geocodeKey}`;
+    const res = await fetch(geocodeUrl);
+    if (!res.ok) {
+      throw new Error("Reverse Geolocation not working");
+    }
+    const data = await res.json();
+    console.log(data);
   },
 
   getLocation: () => {
